0dff8b696fa6d6945d075aaa5aa83daa34f1bc84
Alex Stickler <astickler5@gmail.com>
2017-06-07 11:55:17 -0500

    Occupation addition to search
    Added occupation

--- search.php
+++ search.php
@@ -7,25 +7,25 @@
 	    <link href='http://fonts.googleapis.com/css?family=Lato:400,300italic,700italic' rel='stylesheet' type='text/css'>
 	<script src='js/jquery-1.3.1.min.js'></script>
 			<script src='js/jquery.tablesorter.min.js'></script>
-	
-			
-	
+
+
+
 			<script type="text/javascript">
-			
+
    $(document).ready(function() {
-   
+
      $("#keywords").tablesorter({ headers: {
          2: { sorter: 'digit' } // column number, type
      } });
 });
     </script>
-		
+
 
 	</head>
 	<body>
 		<header>
 			<div class="inner">
-				
+
 				<nav>
 					<ul>
 						<li><a href="index.html">Home</a></li>
@@ -44,34 +44,35 @@
 				</nav>
 			</div>
 		</header>
-		
+
 		<br>
-			
+
 			<?php
 			$name_last=$_GET['name_last'];
 			$interment_at=$_GET['interment_at'];
 			$cause_of_death=$_GET['cause_of_death'];
 			$name_first=$_GET['name_first'];
 			$year=$_GET['year'];
+			$occupation=$_GET['occupation'];
 			$p=$_Get['prim'];
 			$con = mysql_connect("localhost","ernest","xroads66");
-	
+
 			if (!$con)
 			  {
 			  die('Could not connect: ' . mysql_error());
 			  }
-			
-			mysql_select_db("hayesLedgers", $con);
-			
-			
 
-			
-			$result = mysql_query("SELECT * FROM l1 WHERE name_last LIKE '$name_last%' AND cause_of_death LIKE '%$cause_of_death%' AND name_first LIKE '$name_first%' AND year(date_on_ledger) LIKE '%$year%' AND interment_at like '%$interment_at%' ORDER BY age_years ASC");
-			//name_first, name_last, age_years, cause_of_death, interment_at, date_on_ledger, funeral_services_at, 'PRIMARY'                
-			echo 
+			mysql_select_db("hayesLedgers", $con);
+
+
+
+
+			$result = mysql_query("SELECT * FROM l1 WHERE name_last LIKE '$name_last%' AND cause_of_death LIKE '%$cause_of_death%' AND name_first LIKE '$name_first%' AND year(date_on_ledger) LIKE '%$year%' AND interment_at like '%$interment_at%' AND occupation LIKE '%$occupation%' ORDER BY age_years ASC");
+			//name_first, name_last, age_years, cause_of_death, interment_at, date_on_ledger, funeral_services_at, 'PRIMARY'
+			echo
 						"<div id=\"wrapper\">
 		  <h1>Sortable Demo </h1>
-		
+
 		  <table id='keywords' class='tablesorter' cellspacing='0' cellpadding='0'>
 		    <thead>
 		      <tr>
@@ -81,11 +82,12 @@
 			<th><span>Date of Death</span></th>
 			<th><span>Cause of Death</span></th>
 			<th><span>Cemetery</span></th>
+			<th><span>Occupation</span></th>
 		      </tr>
 		    </thead>";
 			while($row = mysql_fetch_array($result)) {
-						
-						echo 
+
+						echo
 		    "
 		      <tr>
 			<td >". "<a href=view.php?id=".$row['prim'].">".$row['name_first']."</td>
@@ -94,28 +96,29 @@
 			<td>".$row['date_on_ledger']."</td>
 			<td>".$row['cause_of_death']."</td>
 			<td>".$row['interment_at']."</td>
+			<td>".$row['occupation']."</td>
 		      </tr>";
-			
-				
-				
-				
-	
+
+
+
+
+
 			  }
-			  
-			  
-			  
-			
-				
-				
-				
-		      
+
+
+
+
+
+
+
+
   echo "</table>";
  echo "</div> ";
-			
+
 		echo json_encode($result);
 			mysql_close($con);
 			?>
-		
-	 
+
+
 	</body>
 	</html>
