b34663a88c36f224ae14f7c5fc879745bf759b4a
babyll <saad1225@gmail.com>
2017-06-14 16:42:31 -0500

    removing excess to test

--- search.php
+++ search.php
@@ -64,10 +64,10 @@
 
 			mysql_select_db("hayesLedgers", $con);
 
-
-
-
-			$result = mysql_query("SELECT * FROM l1 WHERE name_last LIKE '$name_last%' AND cause_of_death LIKE '%$cause_of_death%' AND name_first LIKE '$name_first%' AND year(date_on_ledger) LIKE '%$year%' AND year(date_on_ledger) BETWEEN '$startyear' AND '$endyear' AND interment_at LIKE '%$interment_at%' AND occupation LIKE '%$occupation%' ORDER BY age_years ASC");
+<!--
+name_last LIKE '$name_last%' AND cause_of_death LIKE '%$cause_of_death%' AND name_first LIKE '$name_first%' AND year(date_on_ledger) LIKE '%$year%' AND year(date_on_ledger) BETWEEN '$startyear' AND '$endyear' AND interment_at LIKE '%$interment_at%' AND occupation LIKE '%$occupation%'
+-->
+			$result = mysql_query("SELECT * FROM l1 WHERE year(date_on_ledger) BETWEEN '%$startyear%' AND '%$endyear%' ORDER BY age_years ASC");
 			//name_first, name_last, age_years, cause_of_death, interment_at, date_on_ledger, funeral_services_at, 'PRIMARY'
 			echo
 						"<div id=\"wrapper\">
