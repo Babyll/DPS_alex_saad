de93942cdbcd1b989ca1d3cf903132240411de3b
babyll <saad1225@gmail.com>
2017-06-19 12:24:31 -0500

    adding OR test

--- search.php
+++ search.php
@@ -68,7 +68,9 @@
 name_last LIKE '$name_last%' AND cause_of_death LIKE '%$cause_of_death%' AND name_first LIKE '$name_first%' AND year(date_on_ledger) LIKE '%$year%' AND year(date_on_ledger) BETWEEN '$startyear' AND '$endyear' AND interment_at LIKE '%$interment_at%' AND occupation LIKE '%$occupation%'
 ORDER BY age_years ASC
 -->
-			$result = mysql_query("SELECT * FROM l1 WHERE year(date_on_ledger) BETWEEN '%$startyear%' AND '%$endyear%'");
+			$result = mysql_query("SELECT * FROM l1 WHERE (year(date_on_ledger) BETWEEN '%$startyear%' AND '%$endyear%') OR name_last LIKE '$name_last%' AND cause_of_death LIKE '%$cause_of_death%' AND name_first LIKE '$name_first%' AND year(date_on_ledger) LIKE '%$year%' AND year(date_on_ledger) BETWEEN '$startyear' AND '$endyear' AND interment_at LIKE '%$interment_at%' AND occupation LIKE '%$occupation%'
+ORDER BY age_years ASC
+");
 			//name_first, name_last, age_years, cause_of_death, interment_at, date_on_ledger, funeral_services_at, 'PRIMARY'
 			echo
 						"<div id=\"wrapper\">
